f4893145e209c9cbe43d17ae32168c15f77ffa8d
paksak4 <paksak4@naver.com>
2026-06-01 16:19:38 +0900

    fix: 메인페이지내에서 백엔드 연동 확인

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,3 @@
-// import { useState, useEffect } from 'react'
-// import axios from 'axios'
-//
-// function App() {
-//   const [index, setIndex] = useState('')
-//
-//   useEffect(() => {
-//     const fetchIndex = () => {
-//       axios.get('/api/hello')
-//           .then(res => {
-//             setIndex(res.data)
-//           })
-//           .catch(err => {
-//             console.log(err)
-//           })
-//     }
-//
-//     fetchIndex(); // 초기 실행
-//
-//   }, [])
-//
-//
-//   return (
-//       <div>
-//         백엔드 요청 확인 : {index}
-//       </div>
-//   )
-// }
-
 import { RouterProvider } from 'react-router-dom';
 import router from './router/index';
 
--- frontend/src/pages/Main/MainPage.jsx
+++ frontend/src/pages/Main/MainPage.jsx
@@ -1,7 +1,22 @@
 // 메인페이지
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const MainPage = () => {
-  return <div>메인 화면</div>;
+  const [index, setIndex] = useState('');
+
+  useEffect(() => {
+    axios.get('/api/hello')
+      .then(res => setIndex(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      메인 화면
+      <p>백엔드 요청 확인 : {index}</p>
+    </div>
+  );
 };
 
 export default MainPage;
